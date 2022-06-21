1e8f1a4c85013bf03d253715352bb14ca577e0d9
Stephen HC Lin <linhc130@hotmail.com>
2022-06-21 22:34:01 +0800

    Update index.js
    Copy from eNameCard folder

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -1,13 +1,50 @@
-import './index.css';
-import liff from '@line/liff'
+import flexCard from './liffFlex.js'
 
-document.addEventListener("DOMContentLoaded", function() {
-  liff
-    .init({ liffId: process.env.LIFF_ID })
-    .then(() => {
-        console.log("Success! you can do something with LIFF API here.")
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-});
+async function liffInit() {
+  await liff.init({ liffId: "你-LIFF app-的-LIFF ID" });
+  if (!liff.isLoggedIn()) {
+    liff.login({ redirectUri: window.location.href });
+  }
+}
+
+function createButton(profile, body) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const userImage = profile.pictureUrl;
+  const userReply = [urlParams.get('name'), urlParams.get('phone'), urlParams.get('email'), userImage];
+  const flexContent = flexCard(userReply);
+  const handleClick = async () => {
+    if (liff.isApiAvailable("shareTargetPicker")) {
+      try {
+        const result = await liff.shareTargetPicker([
+          {
+            "type": "flex",
+            "altText": `${urlParams.get('name')} present name card from Netlify`,
+            "contents": flexContent
+          }
+        ])
+
+        if (result) {
+          alert('Flex Message success');
+        }
+
+      } catch (error) {
+        alert("Flex Message got some error");
+      }
+    }
+  }
+
+  const button = document.createElement("button");
+  button.innerHTML = "Share Your Name Card";
+  button.onclick = handleClick;
+
+  body.appendChild(button);
+}
+
+async function main() {
+  await liffInit()
+  const profile = await liff.getProfile();
+  const body = document.querySelector('body');
+  createButton(profile, body);
+}
+
+main()
